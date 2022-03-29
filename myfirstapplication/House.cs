09e275acec0033f6aea9e1a9c8af067011755108
jlathrop <jared.lathrop44@gmail.com>
2022-03-29 11:08:25 -0500

    CAC 103 - CSharp Intro Lab Exercise

---
+++ myfirstapplication/House.cs
@@ -0,0 +1,15 @@
+﻿namespace myfirstapplication;
+
+internal class House
+{
+    string foundation;
+    string roofType;
+    string doorColor;
+    int windowSize;
+
+    public void DoorOpenClose()
+    {
+        Console.WriteLine("My door is open.");
+    }
+
+}
--- myfirstapplication/Program.cs
+++ myfirstapplication/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using myfirstapplication;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// The below statements calls the function
+// single line comment
+HouseExample();
+
+/*
+ Multi line comment
+The below statements is a local function. Local functions are
+inside of members and are private by default. this mens that they are
+specific to that member and can only be called inside
+that member.
+*/
+void HouseExample()
+{
+    House myHouse = new House();
+    myHouse.DoorOpenClose();
+}
+
+// This is for C# Intro Lab Exercise
+
+void MyLocalFunction()
+{
+    Console.WriteLine("This is my first sentence");
+    Console.WriteLine("I am new to CSharp");
+    Console.WriteLine("This CSharp course is cool");
+    Console.WriteLine("I am learning new stuff everyday");
+}
+
+MyLocalFunction();
 
