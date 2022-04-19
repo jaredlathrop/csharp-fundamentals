7e784a8030d8fa68d1ab128ccf9a8624c95e4418
jlathrop <jared.lathrop44@gmail.com>
2022-04-19 17:21:18 -0500

    CAC 102 - Exercise 10 Horse and Boat

---
+++ myfirstapplication/Exercises/Boat.cs
@@ -0,0 +1,64 @@
+﻿namespace myfirstapplication;
+
+internal class Boats
+{
+    /*
+       Create a class based on your boat object. Include your attributes as auto properties.
+    Create 3 constructors. The first constructor should have parameters equal to your properties.
+    Assign each parameter to your properties. The second constructorshould have 2 parameters.
+    Using the :this()keyword, you will pass your information to the first constructor.
+    This includes the 2 parameters and default values. For the last constructor,
+    you will createthe default constructor. Using the :this()keyword, you will
+    pass 2 default valuesin that will pass to the 2ndconstructor.
+    Also include in the class, your method for boat.
+     */
+
+    // properties
+    public string hullShape;
+    public string woodType;
+    public int length;
+
+    public string HullShape
+    {
+        get => hullShape;
+        set => hullShape = value;
+    }
+
+    public string WoodType
+    {
+        get => woodType;
+        set => woodType = value;
+    }
+
+    public int Length
+    {
+        get => length;
+        set => length = value;
+    }
+
+
+    // first constructor
+    public Boats(string shape, string wood, int size)
+    {
+        hullShape = shape;
+        woodType = wood;
+        length = size;
+    }
+
+    // second constructor
+    public Boats(string hullShape, string woodType)
+        :this(hullShape, woodType, 100) { }
+
+
+    // third constructor
+    public Boats()
+        : this("V-Shape", "Oak Wood") { }
+
+    // method
+    public void SailRaise()
+    {
+        Console.WriteLine($"The {woodType} boat with a {hullShape} hull has raised" +
+            $"it's sails");       
+    }
+
+} // end class
---
+++ myfirstapplication/Exercises/Horse.cs
@@ -0,0 +1,42 @@
+﻿namespace myfirstapplication;
+
+internal class Horse
+{
+    /*
+     Create a class based on your Horseobject. Include your attributes as auto properties.
+    Create 3 constructors. The first constructor should have parameters equal to your properties.
+    Assign each parameter to your properties. The second constructorshould have 2 parameters.
+    Using the :this()keyword, you will pass your information to the first constructor.
+    This includes the 2 parameters and default values. For the last constructor,
+    you will createthe default constructor. Using the :this()keyword, you will
+    pass 2 default valuesin that will pass to the 2ndconstructor.
+    Also include in the class, your method for Horse.
+     */
+
+    // properties
+    public string HorseColor { get; init; }
+    public string HorseGender { get; init; }
+    public int HorseHeight { get; init; }
+
+    // 1st constructor
+    public Horse(string horseColor, string horseGender, int horseHeight)
+    {
+        HorseColor = horseColor;
+        HorseGender = horseGender;
+        HorseHeight = horseHeight;
+    }
+
+    // 2nd constructor
+    public Horse(string HorseColor, string HorseGender)
+        :this(HorseColor, HorseGender, 60) { }
+
+    // 3rd constructor
+    public Horse()
+        :this("Black", "Female") { }
+
+    // method
+    public void HorseTrot()
+    {
+        Console.WriteLine($"My {HorseGender} horse is trotting");
+    }
+}
